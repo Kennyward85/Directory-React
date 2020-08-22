cbe7434112a195f373fe1c09229a1076ba7d3ade
Osyrus85 <osyrus85@gmail.com>
2020-08-22 01:03:57 -0500

    Revert "Updates"
    This reverts commit c077ebfc1d88b647834e2ee8d72f19308ebd32bb.

---
+++ src/components/Table/TableSet.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Table from 'react-bootstrap/table';
+
+
+  
+const TableSet = (props) => {
+   
+      return (
+         
+        // Sorting Array in ascending or Descending order on click for all headers but phone number
+        // Using sortArray 
+        <Table striped bordered hover variant='dark'>
+            <thead>
+                <tr>
+                    <th className="first" onClick={props.sort} >First Name</th>
+                    <th className= 'last' onClick= {props.sort}>Last Name</th>
+                    <th className= 'email' onClick={props.sort}>Email</th>
+                    <th className= 'username' onClick={props.sort}>Username</th>
+                    <th className= 'city' onClick={props.sort}>city</th>
+                    <th className= 'state' onClick={props.sort}>State</th>
+                    <th >Phone</th>
+                </tr>
+            </thead>
+            <tbody>
+            {props.users.map(user => {
+                
+            return    <tr key={props.users.id}> 
+                   <td>{user.name.first}</td>
+                   <td>{user.name.last}</td>
+                   <td>{user.email}</td>
+                   <td>{user.login.username}</td>
+                   <td>{user.location.city}</td>
+                   <td>{user.location.state}</td>
+                   <td>{user.phone}</td>
+               </tr>})}
+            </tbody>
+        </Table>
+    )
+}
+
+export default TableSet;
